c2d46d2b485e7107030a4e4cb259483de88fe35b
chen_chen <chen_chen@kunlun-inc.com>
2020-04-20 11:32:11 +0800

    no message

--- src/main/java/jenkins/Test.java
+++ src/main/java/jenkins/Test.java
@@ -2,6 +2,10 @@ package jenkins;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Fuck!!");
+        byte[] b = null;
+        for (int i = 0; i < 30; i++) {
+            b = new byte[20* 1024 * 1024];
+        }
+        System.out.println("over!");
     }
 }
